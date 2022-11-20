c158a24a03bb7958bbe6ee2c68ea7a60ea597ff1
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-11-20 21:31:23 +0100

    frontend work

---
+++ static/lib/components/box/main.js
@@ -0,0 +1,14 @@
+export class Component {
+  constructor(options) {
+    this.document = options.shadowDom;
+    this.box = this.document.getElementById("box");
+  }
+
+  attributeChangedCallback(attribute, oldValue, newValue) {
+    switch (attribute) {
+      case "layout":
+        this.box.className = newValue;
+        break;
+    }
+  }
+}
