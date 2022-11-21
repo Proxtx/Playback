ecfa854169bc58fce452af4c20c1a753485958f9
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-11-21 15:20:35 +0100

    more frontend work. added multiple components components

--- static/lib/components/title/main.js
+++ static/lib/components/title/main.js
@@ -1,1 +1,19 @@
-export class Component {}
+export class Component {
+  constructor(options) {
+    this.document = options.shadowDom;
+    this.title = this.document.getElementById("title");
+    this.subtitle = this.document.getElementById("subtitle");
+  }
+
+  attributeChangedCallback(attribute, oldValue, newValue) {
+    switch (attribute) {
+      case "title":
+        this.title = newValue;
+        break;
+
+      case "subtitle":
+        this.subtitle = newValue;
+        break;
+    }
+  }
+}
